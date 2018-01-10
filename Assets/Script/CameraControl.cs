48a5f503344225a1dd7b431dc7c7c585f4f6d19d
Machida <aya14138@gmail.com>
2018-01-10 11:50:19 +0900

    Playerの動き（左右移動・ジャンプ）。カメラの追従。

---
+++ Assets/Script/CameraControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public GameObject Player;
+
+
+    void Start()
+    {
+        
+
+    }
+
+
+   
+    // Update is called once per frame
+    void Update()
+    {
+        //セットしたPlayerのx座標を基に同じ分量動く(y,zは初期のPlayerの位置)
+        transform.position = new Vector3(Player.transform.position.x, 0, -1);
+
+
+        //Stage外にカメラが追従しないように
+        //右端
+        if (transform.position.x > 0)
+        {
+            transform.position = new Vector3(0, 0, -1);
+        }
+        //左端
+        if (transform.position.x < -50)
+        {
+            transform.position = new Vector3(-50, 0, -1);
+        }
+
+
+    }
+
+
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+
+    //Playerジャンプ制御
+    private Rigidbody2D rd2d;
+    //ジャンプ一回だけの処理flg
+    private bool Jump;
+
+
+
+
+    // Use this for initialization
+    void Start () {
+
+        rd2d = GetComponent<Rigidbody2D>();
+        Jump = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {  //←が押されたら左へ(-=)
+            transform.position -= new Vector3(3.0f, 0.0f, 0.0f) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        { //→が押されたら右へ(+=)
+            transform.position += new Vector3(3.0f, 0.0f, 0.0f) * Time.deltaTime;
+        }
+        //ジャンプキーが押されたらジャンプflgがfalseならジャンプする
+        if (Input.GetKeyDown("space") && Jump == false)
+        {
+            this.rd2d.velocity = Vector2.up * 7.0f;
+            //連続ジャンプさせないようにtrue
+            Jump = true;
+        }
+
+
+    }
+
+
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+        //地面に着いたらflgをfalseにして飛べるようにする
+        if (col.gameObject.tag == "Ground")
+        {
+            Jump = false;
+        }
+
+    }
+
+
+
+
+}
