e1ea4cf0bbb04c34ed6fc95030f75a131bddea3c
Machida <aya14138@gmail.com>
2018-01-31 09:36:21 +0900

    [町田]ステージ作成途中。ギミック追加。

---
+++ Assets/Script/Gimmick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gimmick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("(ﾟ∀ﾟ)");
+        }
+
+
+	}
+}
